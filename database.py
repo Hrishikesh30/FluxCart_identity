bf65a28f63e3bcad461d58adc61eccf304556768
hrishikesh <hrishikesh@gmail.com>
2025-07-03 23:40:21 +0530

    added: DB config , models and req/res schema

---
+++ database.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+#mysql creds 
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:password@localhost:3306/bitespeed"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+ 
